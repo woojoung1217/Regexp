4fa7787ce014bf5047227eb59b409dd28bac69a6
woojoung <ywj981217@naver.com>
2021-10-10 18:09:26 +0900

    정규표현식 study

---
+++ main.js
@@ -0,0 +1,14 @@
+const str =`010-4163-7540
+ywj981217@Naver.com
+ywj1212121.2
+the
+https://fastcampus.co.kr/courses/203720/clips/.
+dfdfdfdtf erelrkerldkr dfkdlfkdfdlfk r dfldrkdflkdflk rldfkd`
+
+
+
+
+
+console.log(
+  str.match(/d$/g)
+  )
